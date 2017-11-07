8d6a4f932230805f6fc712f4f79bb299f7fa245e
Joyce Wong <wong.cy.joyce@gmail.com>
2017-11-07 19:00:12 +0000

    created wikinimous with rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # List articles
+  get "articles", to: "articles#index"
+
+  # Add article
+  get "articles/new", to: "articles#new"
+  post "articles", to: "articles#create"
+
+  # Display a single article
+  get "articles/:id", to: "articles#show", as: :article
+
+  # Update an article
+  get "articles/:id/edit", to: "articles#edit", as: :articles_edit
+  patch "articles/:id", to: "articles#update"
+
+  # Delete an article
+  delete "articles/:id", to: "articles#destroy"
 end
